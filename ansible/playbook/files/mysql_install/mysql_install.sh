37e95ef54e70342ddd07af8ce4582ac7a1e42612
Emerson Gaudencio <emerson.fender@gmail.com>
2021-07-26 01:55:47 +0100

    added support for CentOS 8

--- ansible/playbook/files/mysql_install/mysql_install.sh
+++ ansible/playbook/files/mysql_install/mysql_install.sh
@@ -5,6 +5,54 @@ echo "##############"
 echo "$1" > /tmp/MYSQL_VERSION
 MYSQL_VERSION=$(cat /tmp/MYSQL_VERSION)
 
+# os_type = rhel
+os_type=
+# os_version as demanded by the OS (codename, major release, etc.)
+os_version=
+supported="Only RHEL/CentOS 7 & 8 are supported for this installation process."
+
+msg(){
+    type=$1 #${1^^}
+    shift
+    printf "[$type] %s\n" "$@" >&2
+}
+
+error(){
+    msg error "$@"
+    exit 1
+}
+
+identify_os(){
+    arch=$(uname -m)
+    # Check for RHEL/CentOS, Fedora, etc.
+    if command -v rpm >/dev/null && [[ -e /etc/redhat-release || -e /etc/os-release ]]
+    then
+        os_type=rhel
+        el_version=$(rpm -qa '(oraclelinux|sl|redhat|centos|fedora|rocky|alma|system)*release(|-server)' --queryformat '%{VERSION}')
+        case $el_version in
+            1*) os_version=6 ; error "RHEL/CentOS 6 is no longer supported" "$supported" ;;
+            2*) os_version=7 ;;
+            5*) os_version=5 ; error "RHEL/CentOS 5 is no longer supported" "$supported" ;;
+            6*) os_version=6 ; error "RHEL/CentOS 6 is no longer supported" "$supported" ;;
+            7*) os_version=7 ;;
+            8*) os_version=8 ;;
+             *) error "Detected RHEL or compatible but version ($el_version) is not supported." "$supported"  "$otherplatforms" ;;
+         esac
+         if [[ $arch == aarch64 ]] && [[ $os_version != 7 ]]; then error "Only RHEL/CentOS 7 are supported for ARM64. Detected version: '$os_version'"; fi
+    fi
+
+    if ! [[ $os_type ]] || ! [[ $os_version ]]
+    then
+        error "Could not identify OS type or version." "$supported"
+    fi
+}
+
+### OS auto discovey to identify which is the OS and version thats been used it.
+identify_os
+
+echo $os_type
+echo $os_version
+
 ##### FIREWALLD DISABLE ########################
 systemctl disable firewalld
 systemctl stop firewalld
@@ -19,14 +67,17 @@ rm -rf /etc/yum.repos.d/mariadb.repo
 rm -rf /etc/yum.repos.d/mysql-community.repo
 rm -rf /etc/yum.repos.d/mysql-community-source.repo
 rm -rf /etc/yum.repos.d/percona-original-release.repo
-yum clean headers
-yum clean packages
-yum clean metadata
+yum clean all
 
 ####### PACKAGES ###########################
-# -------------- For RHEL/CentOS 7 --------------
-yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-# yum -y install epel-release
+if [[ $os_type == "rhel" ]]; then
+    # yum -y install epel-release
+    if [[ $os_version == "7" ]]; then
+      yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    elif [[ $os_version == "8" ]]; then
+      yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+    fi
+fi
 
 ### remove old packages ####
 yum -y remove mariadb-libs
@@ -39,47 +90,86 @@ yum -y remove MariaDB-common MariaDB-compat
 yum -y remove MariaDB-server MariaDB-client
 yum -y remove percona-release
 
-### install pre-packages ####
-yum -y install yum-utils screen expect nload bmon iptraf glances perl perl-DBI openssl pigz zlib file sudo  libaio rsync snappy net-tools wget nmap htop dstat sysstat perl-IO-Socket-SSL perl-Digest-MD5 perl-TermReadKey socat libev gcc zlib zlib-devel openssl openssl-devel python-pip python-devel zip unzip
+### clean yum cache ###
+yum clean all
+
+### monitoring pre-packages ####
+yum -y install nload bmon iptraf glances nmap htop dstat sysstat socat
+
+# dev tools
+yum -y install screen yum-utils expect perl perl-DBI perl-IO-Socket-SSL perl-Digest-MD5 perl-TermReadKey  libev gcc zlib zlib-devel openssl openssl-devel python3 python3-pip python3-devel
+
+# others pre-packages
+yum -y install pigz zlib file sudo libaio rsync snappy net-tools wget
+
+### clean yum cache ###
+yum clean all
 
 #### REPO MYSQL ######
-# -------------- For RHEL/CentOS 7 --------------
-#### https://dev.mysql.com/downloads/repo/yum/
-if [ "$MYSQL_VERSION" == "80" ]; then
-   yum install https://repo.percona.com/yum/percona-release-latest.noarch.rpm -y
-   sudo percona-release setup ps80
-   yum -y install percona-server-server percona-server-client percona-server-shared percona-server-shared-compat
- elif [[ "$MYSQL_VERSION" == "57" ]]; then
-   yum install https://repo.percona.com/yum/percona-release-latest.noarch.rpm -y
-   yum -y install Percona-Server-client-$MYSQL_VERSION
-   yum -y install Percona-Server-devel-$MYSQL_VERSION
-   yum -y install Percona-Server-server-$MYSQL_VERSION
-   yum -y install Percona-Server-shared-$MYSQL_VERSION
-   yum -y install Percona-Server-shared-compat-$MYSQL_VERSION
- elif [[ "$MYSQL_VERSION" == "56" ]]; then
-   yum install https://repo.percona.com/yum/percona-release-latest.noarch.rpm -y
-   yum -y install Percona-Server-client-$MYSQL_VERSION
-   yum -y install Percona-Server-devel-$MYSQL_VERSION
-   yum -y install Percona-Server-server-$MYSQL_VERSION
-   yum -y install Percona-Server-shared-$MYSQL_VERSION
-   yum -y install Percona-Server-shared-compat-$MYSQL_VERSION
+if [[ $os_type == "rhel" ]]; then
+  if [[ $os_version == "7" ]]; then
+    if [ "$MYSQL_VERSION" == "80" ]; then
+       yum install https://repo.percona.com/yum/percona-release-latest.noarch.rpm -y
+       percona-release setup -y ps80
+       yum -y install percona-server-server percona-server-client percona-server-shared percona-server-shared-compat
+     elif [[ "$MYSQL_VERSION" == "57" ]]; then
+       yum install https://repo.percona.com/yum/percona-release-latest.noarch.rpm -y
+       percona-release setup -y ps57
+       yum -y install Percona-Server-client-$MYSQL_VERSION
+       yum -y install Percona-Server-devel-$MYSQL_VERSION
+       yum -y install Percona-Server-server-$MYSQL_VERSION
+       yum -y install Percona-Server-shared-$MYSQL_VERSION
+       yum -y install Percona-Server-shared-compat-$MYSQL_VERSION
+     elif [[ "$MYSQL_VERSION" == "56" ]]; then
+       yum install https://repo.percona.com/yum/percona-release-latest.noarch.rpm -y
+       percona-release setup -y ps56
+       yum -y install Percona-Server-client-$MYSQL_VERSION
+       yum -y install Percona-Server-devel-$MYSQL_VERSION
+       yum -y install Percona-Server-server-$MYSQL_VERSION
+       yum -y install Percona-Server-shared-$MYSQL_VERSION
+       yum -y install Percona-Server-shared-compat-$MYSQL_VERSION
+    fi
+  elif [[ $os_version == "8" ]]; then
+    if [ "$MYSQL_VERSION" == "80" ]; then
+       yum install https://repo.percona.com/yum/percona-release-latest.noarch.rpm -y
+       percona-release setup -y ps80
+       sed -ie 's/enabled=1/enabled=1\nmodule_hotfixes=1/g' /etc/yum.repos.d/percona-ps-80-release.repo
+       yum -y install percona-server-server percona-server-client percona-server-shared percona-server-shared-compat
+     elif [[ "$MYSQL_VERSION" == "57" ]]; then
+       yum install https://repo.percona.com/yum/percona-release-latest.noarch.rpm -y
+       percona-release setup -y ps57
+       sed -ie 's/enabled=1/enabled=1\nmodule_hotfixes=1/g' /etc/yum.repos.d/percona-ps-57-release.repo
+       yum -y install Percona-Server-client-$MYSQL_VERSION
+       yum -y install Percona-Server-devel-$MYSQL_VERSION
+       yum -y install Percona-Server-server-$MYSQL_VERSION
+       yum -y install Percona-Server-shared-$MYSQL_VERSION
+       yum -y install Percona-Server-shared-compat-$MYSQL_VERSION
+     elif [[ "$MYSQL_VERSION" == "56" ]]; then
+       error "Could not install MySQL 56 because it is not supported on RHEL/CentOS 8."
+    fi
+  fi
 fi
 
 ### installation mysql add-ons via yum ####
 yum -y install perl-DBD-MySQL
-yum -y install MySQL-python
 
-### clean yum cache ###
-yum clean headers
-yum clean packages
-yum clean metadata
+####### PACKAGES ###########################
+if [[ $os_type == "rhel" ]]; then
+    if [[ $os_version == "7" ]]; then
+      # -------------- For RHEL/CentOS 7 --------------
+      #### mydumper ######
+      yum -y install https://github.com/maxbube/mydumper/releases/download/v0.10.7-2/mydumper-0.10.7-2.el7.x86_64.rpm
 
-#### mydumper ######
-#yum -y install https://github.com/maxbube/mydumper/releases/download/v0.9.5/mydumper-0.9.5-2.el7.x86_64.rpm
-yum -y install https://github.com/emersongaudencio/linux_packages/raw/master/RPM/mydumper-0.9.5-2.el7.x86_64.rpm
+      #### qpress #####
+      yum -y install https://github.com/emersongaudencio/linux_packages/raw/master/RPM/qpress-11-1.el7.x86_64.rpm
+    elif [[ $os_version == "8" ]]; then
+      #### mydumper ######
+      yum -y install https://github.com/maxbube/mydumper/releases/download/v0.10.7-2/mydumper-0.10.7-2.el8.x86_64.rpm
 
-#### qpress #####
-yum -y install https://github.com/emersongaudencio/linux_packages/raw/master/RPM/qpress-11-1.el7.x86_64.rpm
+      #### qpress #####
+      yum -y install https://repo.percona.com/tools/yum/release/8/RPMS/x86_64/qpress-11-1.el8.x86_64.rpm
+    fi
+fi
 
 ### Percona #####
 ### https://www.percona.com/doc/percona-server/LATEST/installation/yum_repo.html
