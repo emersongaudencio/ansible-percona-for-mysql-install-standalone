881321912dfe8634ad2edbd6efcf20ec8a6309ba
Emerson Gaudencio <emerson.fender@gmail.com>
2021-11-25 01:20:37 +0000

    add support to install mysql 8 minor versions

--- ansible/run_mysql_install.sh
+++ ansible/run_mysql_install.sh
@@ -8,6 +8,7 @@ cd $SCRIPT_PATH
 
 VAR_HOST="$1"
 VAR_MYSQL_VERSION="$2"
+VAR_MYSQL_MINOR_VERSION="$3"
 
 if [ "${VAR_HOST}" == '' ] ; then
   echo "No host specified. Please have a look at README file for futher information!"
@@ -24,7 +25,7 @@ if [ "$VAR_MYSQL_VERSION" -gt 0 -a "$VAR_HOST" != "" ]; then
   ansible -i $SCRIPT_PATH/hosts -m ping $VAR_HOST -v
 
   ### mysql install ####
-  ansible-playbook -v -i $SCRIPT_PATH/hosts -e "{mysql_version: '$VAR_MYSQL_VERSION'}" $SCRIPT_PATH/playbook/mysql_install.yml -l $VAR_HOST
+  ansible-playbook -v -i $SCRIPT_PATH/hosts -e "{mysql_version: '$VAR_MYSQL_VERSION', mysql_minor_version: '$VAR_MYSQL_MINOR_VERSION'}" $SCRIPT_PATH/playbook/mysql_install.yml -l $VAR_HOST
 else
   echo "Sorry, this script must have 2 parameters to run. So first of all you have to fill up the first parameter with the ansible hostname and the second parameter MySQL version, please have a look at README file for futher information!"
   exit 1
